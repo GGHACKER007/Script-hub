9c1a8d21560295395b918d905ee41c069904a0b0
GGHACKER007 <141738890+GGHACKER007@users.noreply.github.com>
2023-12-10 19:35:57 +0530

    Add files via upload

---
+++ VIP MENU.lua
@@ -0,0 +1,668 @@
+DATA = 0
+gg.sleep(100)
+gg.alert([[
+Test project of TEAM ANM implementing token systems.
+We will add 24/7 random tokens for each device soon as possible]])
+if gg.alert("This script contents key thought linkvertise have you got the key? if yes then click yes, if no then click get key", "YES", "GET KEY") ~= 1 then
+gg.setVisible(false)
+gg.clearResults()
+gg.clearList()
+gg.copyText("https://direct-link.net/872138/test-key-system")
+gg.alert("Copied to clipboard now open any browser & paste the link complete the linkvertise")
+gg.setVisible(true)
+print([[
+
+Link has been copied to your clipboard open any browser & paste the link complete the verification & get the key!
+
+If you got any error or issue contact me.
+
+Instagram : gg_hacker_cpm < < active > >
+discord : gghacker < < currently inactive > >
+]])
+return os.exit()
+end
+
+
+function Login()
+    local password = "JpewgByjTIeH37mvi0WhHiyUE0LvgKNEIK9QKuNbKfrR2GcBrghd9qO46SovGYYQNz6qY29r8CKgGA1jkNsj0xZ8NTesHt63kvSrfrPKeMdnLjz6KLaqPFkiBJJ24f2f"
+    local userInput
+    while (userInput == nil) do
+    userInput = gg.prompt({"Enter Key"},nil,{'text'});
+    end
+    if userInput[1] ~= password then
+        local faiedLoginHandler = gg.alert("!! Wrong key!!","Try Again",nil,"Exit")
+        
+        if faiedLoginHandler == 1 then
+            Login()
+        else
+           print("Try again after you get the key")
+            os.exit()
+        end
+
+    end
+end
+Login()
+
+
+
+
+
+gg.alert("Correct key!")
+gg.sleep(500)
+if gg.alert("Tired of ads thought key?. Get ads less & keyless for all other upcoming script in the premium subscription permanently for $5", "Enter", "Copy Link") ~= 1 then
+gg.copyText("www.buymeacoffee.com/gg_hacker")
+gg.alert("Copied to clipboard")
+end
+
+gg.sleep(8000)
+gg.setVisible(false) -- Visibility
+-- Function to display a 360° rotating loading animation with more rounds
+function show360RotatingLoadingWithRounds(rounds)
+    local emojis = {"⭕", "⭗", "⭑", "⭒", "⭓", "⭔", "⭕"}  -- Circular pattern of emojis
+    local loadingDuration = 3000  -- Default loading duration in milliseconds
+
+    for round = 0, rounds do
+        for _, emoji in ipairs(emojis) do
+            gg.toast(emoji)
+            gg.sleep(150)  -- Adjust the speed as needed
+        end
+    end
+
+    -- Example usage: Load your script after the loading animation
+    gg.toast("Loading your script...")
+    gg.sleep(loadingDuration)
+    gg.toast("Script loaded!")
+end
+
+-- Example usage with rounds
+show360RotatingLoadingWithRounds(10)
+
+
+-- Patch function
+local isPatched = true
+
+
+gg.sleep(6000)
+
+gg.setVisible(false) -- visibility
+gg.alert("🔴 ENCRYPTED MADE BY\n\n-GG HACKER & TEAM ANM©️Copyright\n\n🇧🇷GG HACKER🇧🇷\n\n⚫TEAM ANM⚫\n________________________________________________","🔴")
+ -- Alert function
+gg.sleep(50) -- timer
+
+gg.alert("╭━━━━┳━━━┳━━━┳━╮╭━╮╭━━━┳━╮╱╭┳━╮╭━╮\n┃╭╮╭╮┃╭━━┫╭━╮┃┃╰╯┃┃┃╭━╮┃┃╰╮┃┃┃╰╯┃┃\n╰╯┃┃╰┫╰━━┫┃╱┃┃╭╮╭╮┃┃╱┃┃╭╮╰╯┃╭╮╭╮┃\n╱╱┃┃╱┃╭━━┫╰━╯┃┃┃┃┃┃┃╰━╯┃┃╰╮┃┃┃┃┃┃\n╱╱┃┃╱┃╰━━┫╭━╮┃┃┃┃┃┃╃╭━╮┃╃╱┃┃┃┃┃┃┃\n╱╱╰╯╱╰━━━┻╯╱╰┻╯╰╯╰╯╰╯╱╰┻╯╱╰━┻╯╰╯╰╯")
+
+
+
+
+-- Script version Free ;)
+-- Game : car parking multiplayer
+-- Version : 4.8.14.8
+-- Source will be provided soon in GitHub
+-- Team ANM
+
+
+
+
+
+
+
+
+---- Main loader page
+gg.setVisible(false) -- Visibility
+if gg.alert("ꔼ━━━━━━━━━━━━━━━━━━━━━━━ꘁ\n𝚂𝙲𝚁𝙸𝙿𝚃 𝙱𝚈 𝚃𝙴𝙰𝙼 𝙰𝙽𝙼 \nꔼ━━━━━━━━━━━━━━━━━━━━━━━ꘁ\n𝙾𝚆𝙽𝙴𝚁 : 𝙶𝙶 𝙷𝙰𝙲𝙺𝙴𝚁\n━━━━━━━━━━━━━━━━━━━━━━━━━\n𝙰𝚙𝚙 𝚗𝚊𝚖𝚎 : " .. gg.getTargetInfo()["label"] .. " \n𝚅𝚎𝚛𝚜𝚒𝚘𝚗 : " .. gg.getTargetInfo()["versionName"] .. " [" .. gg.getTargetInfo()["versionCode"] .. "]\n𝚂𝚌𝚛𝚒𝚙𝚝 𝚟𝚎𝚛𝚜𝚒𝚘𝚗 : 𝚄𝚗𝚜𝚝𝚊𝚋𝚕𝚎\n-------------------------\n" .. "", "🅾🅺", "🅴🆇🅸🆃") ~= 1 then
+
+gg.setVisible(false)
+
+gg.clearResults()
+
+gg.clearList()
+
+
+
+gg.toast(" 𝚃𝙷𝙰𝙽𝙺 𝚈𝙾𝚄")
+
+gg.clearResults()gg.clearList()
+
+print(os.date("🄳🄾🄽🅃 🄵🄾🅁🄶🄴🅃 🅃🄾 🅂🅄🄱🅂🄲🅁🄸🄱🄴 \n\n⏱️ Today : %Y/%m/%d Time: %H:%M:%S ⏱️\n\n"))
+
+print("        ➧ Ⓢⓒⓡⓘⓟⓣ Ⓑⓨ ➠\n\n                       𝙼𝚈 𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻   : 𝙶𝙶 𝙷𝙰𝙲𝙺𝙴𝚁\n\n                                               ━─━───────────༺ᴇxɪᴛ༻──────────━─━")  
+
+gg.sleep(500)
+gg.setVisible(true)
+  os.exit()
+  end
+
+  
+
+
+
+
+
+gg.setVisible(false)
+
+
+
+gg.toast("🛡️ 𝙿𝚁𝙾𝚃𝙴𝙲𝚃𝙴𝙳 𝙱𝚈 𝙶𝙶 𝙷𝙰𝙲𝙺𝙴𝚁 🛡️")
+gg.clearResults()
+
+
+gg.sleep(4000)
+gg.alert([[
+༺♥༻❀༺♥༻
+
+Credits : TEAM ANM
+Script language : Lua
+Supported : 64bit
+Project no : 53
+
+༺♥༻❀༺♥༻
+]], "🆃🅴🅰🅼 🅰🅽🅼")
+
+
+
+
+SENGGOLDONGGG = 2 
+
+
+
+
+-- Function to display a longer duration loading animation
+function showLongLoading()
+    local loadingDuration = 5000  -- Longer loading duration in milliseconds
+
+    gg.toast("Loading...")
+    gg.sleep(1500)  -- Initial delay
+
+    for i = 1, 8 do  -- Display for 3 rounds
+        gg.toast("Loading.")
+        gg.sleep(800)
+        gg.toast("Loading..")
+        gg.sleep(800)
+        gg.toast("Loading...")
+        gg.sleep(800)
+    end
+
+    -- Example usage: Load your main menu after the loading animation
+    gg.toast("Loading main menu...")
+    gg.sleep(loadingDuration)
+    gg.toast("Main menu loaded!")
+end
+
+-- Example usage
+showLongLoading()
+
+gg.sleep(5000)
+gg.setVisible(false)
+function mainMenu()
+local setVis=gg.setVisible
+local isVis=gg.isVisible
+local showmenu=true
+setVis(false)
+menu = gg.choice({
+'╚»💸«╝• 𝚂𝙴𝙽𝙳 𝙼𝙾𝙽𝙴𝚈 』',
+'╚»🔑«╝• 𝙱𝚈𝙿𝙰𝚂𝚂 𝚆𝙾𝚁𝙻𝙳 𝚂𝙰𝙻𝙴 』',
+'╚»🏁«╝• 𝙱𝚄𝙶 𝚁𝙰𝙲𝙴 』',
+'╚»🔓«╝• 𝚄𝙽𝙻𝙾𝙲𝙺 𝙲𝙰𝚁 𝙳𝙾𝙾𝚁 』',
+'╚»🌛«╝• 𝙲𝚘𝚒𝚗 𝚑𝚊𝚌𝚔 𝟸𝟶𝟸𝟹 』',
+'╚»🥵«╝• 𝙿𝚊𝚒𝚍 𝚆𝚑𝚎𝚎𝚕 』',
+'╚»💾«╝• 𝙲𝚑𝚊𝚗𝚐𝚎 𝙸𝚍 • 𝙿𝚎𝚛𝚖𝚊𝚗𝚎𝚗𝚝 • 』',
+'╚»🤫«╝• 𝚂𝚌𝚊𝚖 』× Patched ×',
+'╚»🚨«╝• 𝚄𝚗𝚕𝚘𝚌𝚔 𝚂𝚒𝚛𝚎𝚗 』',
+'╚»😫«╝• 𝙱𝚛𝚞𝚗 𝚘𝚞𝚝 On 』',
+'╚»😫«╝• 𝙱𝚛𝚞𝚗 𝚘𝚞𝚝 Off 』',
+'╚»😈«╝• Death end On 』',
+'╚»😈«╝• Death end Off 』',
+'╚»📎«╝• 𝚄𝚙𝚍𝚊𝚝𝚎𝚜 』',
+'╚»📎«╝• 𝙰𝙱𝙾𝚄𝚃 𝚄𝚂 』',
+'╚»•ᴇxɪᴛ•«╝'},
+nil, ("┏━━━━━━━━༻❁༺━━━━━━━━┓\n    ❀ 𝚂𝙲𝚁𝙸𝙿𝚃 𝙱𝚈 𝚃𝙴𝙰𝙼 𝙰𝙽𝙼 ❀") .. os["date"]("\n ➣ ☀️𝚃𝚘𝚍𝚊𝚢 : %d %B %Y\n ➣ ⌚𝚃𝚒𝚖𝚎 : %X %p\n┗━━━━━━━━༻❁༺━━━━━━━━┛"))
+
+gg.toast("TEAM ANM")
+if menu == nil then
+else
+if menu == 1 then VVIP() end
+if menu == 2 then VVIP1() end
+if menu == 3 then VVIP2() end
+if menu == 4 then VVIP3() end
+if menu == 5 then VVIP4() end
+if menu == 6 then VVIP5() end
+if menu == 7 then VVIP6() end
+if menu == 8 then VVIP7() end
+if menu == 9 then VVIP8() end
+if menu == 10 then VVIP9() end
+if menu == 11 then VVIP10() end
+if menu == 12 then VVIP11() end
+if menu == 13 then VVIP12() end
+if menu == 14 then updates() end
+if menu == 15 then about() end
+if menu == 16 then BYE() end
+end
+end
+
+
+
+
+
+
+function VVIP()
+gg.clearResults()
+gg.clearList()
+gg.setVisible(false)
+if gg.alert("USE THIS HACK IN ROOM NOT LOBBY ONCE THE HACK WAS ACTIVATED SEND $800 CASH TO PLAYES", "🅶🅾", "🆁🅴🆃🆄🆁🅽") ~= 1 then
+gg.setVisible(false)
+gg.clearResults()
+gg.clearList()
+else
+gg.clearResults()gg.clearList()
+gg.setVisible(false)
+ gg.setRanges(gg.REGION_CODE_APP)
+ gg.searchNumber("1,202,590,843,160", gg.TYPE_QWORD)
+ gg.processResume()
+ results = gg.getResults(1000)
+ count = gg.getResultsCount()
+  attack = gg.getResults(count)
+   local copy = false
+   for i, v in ipairs(attack) do
+   v.address=v.address + 0x1B0
+   v.value = '9,855,353'
+   v.flags = gg.TYPE_DWORD
+   gg.setValues(attack)
+   gg.addListItems(attack)
+   gg.getListItems(attack)
+   gg.clearResults()
+gg.clearList()
+gg.sleep(50)
+gg.toast("🔰DONE🔰")
+end
+end
+end
+
+
+
+
+
+
+function VVIP1()
+gg.clearResults()
+gg.clearList()
+-- main code on
+gg.setRanges(gg.REGION_CODE_APP)
+     LibStart=gg.getRangesList('libil2cpp.so')[2].start
+GH=nil
+GH={}
+GH[1]={}
+GH[2]={}
+GH[1].address=LibStart+0x1B4917C
+GH[1].value='h200080D2'
+GH[1].flags=4
+GH[2].address=LibStart+(0x1B4917C+0x4)
+GH[2].value='hC0035FD6'
+GH[2].flags=4
+gg.setValues(GH)
+      gg.alert("OPEN LEVELS & SELECT ANY LEVEL THEN CLICK MENU")
+      gg.toast("🔰DONE🔰")
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+function VVIP2()
+gg.clearResults()
+gg.clearList()
+-- main code on
+gg.setRanges(gg.REGION_CODE_APP)
+     LibStart=gg.getRangesList('libil2cpp.so')[2].start
+GH=nil
+GH={}
+GH[1]={}
+GH[2]={}
+GH[1].address=LibStart+0x207AF10
+GH[1].value='h200080D2'
+GH[1].flags=4
+GH[2].address=LibStart+(0x207AF10+0x4)
+GH[2].value='hC0035FD6'
+GH[2].flags=4
+gg.setValues(GH)
+      gg.toast("🔰DONE🔰")
+    end
+
+
+
+
+
+
+
+function VVIP3()
+gg.clearResults()
+gg.clearList()
+-- main code on
+gg.setRanges(gg.REGION_CODE_APP)
+     LibStart=gg.getRangesList('libil2cpp.so')[2].start
+GH=nil
+GH={}
+GH[1]={}
+GH[2]={}
+GH[1].address=LibStart+0x1D26F94
+GH[1].value='h200080D2'
+GH[1].flags=4
+GH[2].address=LibStart+(0x1D26F94+0x4)
+GH[2].value='hC0035FD6'
+GH[2].flags=4
+gg.setValues(GH)
+      gg.toast("🔰DONE🔰")
+    end
+
+
+
+
+
+
+
+function VVIP4()
+gg.setVisible(false)
+if gg.alert([[
+I'm not sure that it will work or not cuz i didn't tested it before i upload i was in rush so test it & if it doesn't work or got any error.
+
+contact me 
+Instagram : gg_hacker_cpm]], "🅶🅾", "🆁🅴🆃🆄🆁🅽") ~= 1 then
+gg.setVisible(false)
+gg.clearResults()
+gg.clearList()
+else
+gg.setVisible(false)
+gg.setRanges(gg.REGION_ANONYMOUS) -- Memory range
+gg.searchNumber("0;30;50;100;500", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0) -- Searching value
+gg.processResume()
+revert = gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil) -- Results
+gg.editAll("-1900000000", gg.TYPE_DWORD) -- editing value
+gg.clearResults()
+gg.alert("➣ NOW EXIT THE EMOTE STORE & AGAIN GOTO THE EMOTE STORE") -- alert
+gg.alert("➣ NOW BUY ANY 2 EMOTES WHICH HAS NEGATIVE COINS")
+gg.toast("🔰DONE🔰") -- toast
+end
+end
+
+
+function VVIP5()
+gg.clearResults()
+gg.clearList()
+-- main code on
+gg.setRanges(gg.REGION_CODE_APP)
+     LibStart=gg.getRangesList('libil2cpp.so')[2].start
+GH=nil
+GH={}
+GH[1]={}
+GH[2]={}
+GH[1].address=LibStart+0x1AD9C7C
+GH[1].value='h200080D2'
+GH[1].flags=4
+GH[2].address=LibStart+(0x1AD9C7C+0x4)
+GH[2].value='hC0035FD6'
+GH[2].flags=4
+gg.setValues(GH)
+      gg.alert("Now you can buy any wheels")
+      gg.toast("🔰DONE🔰")
+    end
+    
+    
+    
+    
+
+function VVIP6()
+gg.clearResults()
+gg.clearList()
+gg.setVisible(false)
+if gg.alert("MAKE SURE TO SAVE & LOGOUT YOUR ACCOUNT BEFORE USING THIS OPTION\n\n IF YOUR ALREADY SAVED & LOGGED OUT CLICK GO & IF YOU DIDN'T LOGGED OUT CLICK RETURN & LOGOUT", "🅶🅾", "🆁🅴🆃🆄🆁🅽") ~= 1 then
+gg.setVisible(false)
+gg.clearResults()
+gg.clearList()
+else
+gg.clearResults()gg.clearList()
+-- main code on
+gg.setRanges(gg.REGION_CODE_APP)
+     LibStart=gg.getRangesList('libil2cpp.so')[2].start
+GH=nil
+GH={}
+GH[1]={}
+GH[2]={}
+GH[1].address=LibStart+0x32086E4
+GH[1].value='h200080D2'
+GH[1].flags=4
+GH[2].address=LibStart+(0x32086E4+0x4)
+GH[2].value='hC0035FD6'
+GH[2].flags=4
+gg.setValues(GH)
+      gg.alert("Now login back your acc, you can see that your id was changed to make it permanent just save")
+      gg.toast("🔰DONE🔰")
+    end
+    end
+
+
+
+
+
+
+
+
+
+
+function VVIP7()
+if isPatched then
+gg.alert([[
+Scam has been patched, stay tuned soon we try to find new one!
+
+TEAM ANM]], "🆁🅴🆃🆄🆁🅽")
+return
+end
+
+p1=gg.prompt({"ENTER CAR PRICE"},
+{[1]=" "},
+{[1]="number"})
+if p1 == nil then
+gg.toast("Cancel ")
+  else
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber(p1[1].. "X4" ,gg.TYPE_DWORD)
+  p2=gg.prompt({"EDIT YOUR CUSTOM PRICE"},
+    {[1]="0"},
+    {[1]="number"})
+  if p2 == nil then
+gg.toast("Cancel ")
+  else
+  gg.getResults(999)
+  gg.editAll(p2[1].. "X4" ,gg.TYPE_DWORD)
+  gg.clearResults()
+  gg.clearList()
+  gg.toast("🔰DONE🔰")
+end
+end
+end
+
+
+
+
+
+
+function VVIP8()
+gg.clearResults()
+gg.clearList()
+gg.setVisible(false)
+if gg.alert("We can't turn on the siren it more like a visual siren we can't use it but everyone can see it", "🅶🅾", "🆁🅴🆃🆄🆁🅽") ~= 1 then
+gg.setVisible(false)
+gg.clearResults()
+gg.clearList()
+else
+gg.clearResults()gg.clearList()
+LibStart=gg.getRangesList('libil2cpp.so')[2].start
+GH=nil
+GH={}
+GH[1]={}
+GH[2]={}
+GH[1].address=LibStart+0x20DD368
+GH[1].value='h200080D2'
+GH[1].flags=4
+GH[2].address=LibStart+(0x20DD368+0x4)
+GH[2].value='hC0035FD6'
+GH[2].flags=4
+gg.setValues(GH)
+gg.toast("🔰DONE🔰")
+end
+end
+
+
+function VVIP9()
+LibStart = gg.getRangesList("libil2cpp.so")[2].start
+        GH = {
+            {address = LibStart + 85857804, value = "h00C07944", flags = 4}
+        }
+        gg.setValues(GH)
+        gg.toast("🔰DONE🔰")
+        end
+        
+        
+function VVIP10()
+LibStart = gg.getRangesList("libil2cpp.so")[2].start
+        GH = {
+            {address = LibStart + 85857804, value = "h17B7D138", flags = 4}
+        }
+        gg.setValues(GH)
+        gg.toast("🔰DONE🔰")
+        end
+
+
+
+function VVIP11()
+        LibStart = gg.getRangesList("libil2cpp.so")[2].start
+        GH = {
+            {address = LibStart + 85857748, value = "h0000F041", flags = 4},
+            {address = LibStart + 85857752, value = "h0000F0C1", flags = 4}
+        }
+        gg.setValues(GH)
+        gg.toast("🔰DONE🔰")
+        end
+        
+        
+        
+function VVIP12()
+LibStart = gg.getRangesList("libil2cpp.so")[2].start
+        GH = {
+            {address = LibStart + 85857748, value = "h00409CC6", flags = 4},
+            {address = LibStart + 85857752, value = "h00409C46", flags = 4}
+        }
+        gg.setValues(GH)
+        gg.toast("🔰DONE🔰")
+        end
+
+
+
+
+
+
+function updates()
+gg.alert([[
+[+] Updated Bypass world sale
+[+] Added Id changer
+[~] Fixed Crashing
+[~] Fixed errors
+[+] Added a test key, It doesnt has expiration/rest
+[+] Ip whitelist will be added soon
+[+] Premium membership whitelist will be added soon
+
+More options will be added soon!
+
+Credits : TEAM ANM
+]], "🆁🅴🆃🆄🆁🅽")
+end
+
+
+
+
+
+
+
+
+function about()
+gg.alert([[
+GG HACKER : CEO OF TEAM ANM
+Ivão Moniz : CO-OWNER
+Soraia Cordeiro : SCRIPTER, HEAD ADMIN
+Steven Parrish : SCRIPTER, WEB DESIGNER
+Yasuaki Tsuji : JAVASCRIPTER, SITE MANAGER
+Víctor Magalhães : PAYMENT METHODS MANAGER
+Claúdia da Silva : MODERATOR
+
+TEAM ANM : We create Roblox scripts & sell it all our Script for paid users & script is not free!!
+All the script were private it cant be shared with public users!
+Currently scripthub & website has been shutdown due to BYFRON anticheat
+
+Thanks to all other staffs
+]], "🆁🅴🆃🆄🆁🅽")
+end
+
+
+
+
+
+
+
+
+
+
+
+function BYE()
+gg.setVisible(true)
+gg.toast("❤️𝚃𝙷𝙰𝙽𝙺 𝚈𝙾𝚄 𝙵𝙾𝚁 𝚄𝚂𝙸𝙽𝙶❤️")
+print(os.date("🄳🄾🄽🅃 🄵🄾🅁🄶🄴🅃 🅃🄾 🅂🅄🄱🅂🄲🅁🄸🄱🄴 \n\n⏱️ Today : %Y/%m/%d Time: %H:%M:%S ⏱️\n\n"))
+print([[
+
+╭━━━━┳━━━┳━━━┳━╮╭━╮╭━━━┳━╮╱╭┳━╮╭━╮
+┃╭╮╭╮┃╭━━┫╭━╮┃┃╰╯┃┃┃╭━╮┃┃╰╮┃┃┃╰╯┃┃
+╰╯┃┃╰┫╰━━┫┃╱┃┃╭╮╭╮┃┃┃╱┃┃╭╮╰╯┃╭╮╭╮┃
+╱╱┃┃╱┃╭━━┫╰━╯┃┃┃┃┃┃┃╰━╯┃┃╰╮┃┃┃┃┃┃┃
+╱╱┃┃╱┃╰━━┫╭━╮┃┃┃┃┃┃┃╭━╮┃┃╱┃┃┃┃┃┃┃┃
+╱╱╰╯╱╰━━━┻╯╱╰┻╯╰╯╰╯╰╯╱╰┻╯╱╰━┻╯╰╯╰╯
+
+]])
+
+
+
+
+return os.exit()
+end
+
+
+
+
+
+
+
+
+
+
+
+function stopClose()
+while true do
+mainMenu()
+gg.setVisible(false)
+while gg.isVisible() == false do
+end
+end
+end
+
+
+
+
+
+if SENGGOLDONGGG == 2 then
+stopClose()
+else
+UI()
+end
